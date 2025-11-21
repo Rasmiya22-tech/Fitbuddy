80ea172474fae94e0bd7d08005e60ac2fb615bc6
Rasmiya22-tech <arasmiya77@gmail.com>
2025-11-21 11:08:08 +0530

    Added App.js changes

--- App.js
+++ App.js
@@ -1,25 +1,3 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hi I am Rasmiya</Text>
-    
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#944545ff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
